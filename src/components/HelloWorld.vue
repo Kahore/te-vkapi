b6ca03fb0d270f0b52fc70d5c03cec890858b5d9
Konstantin Golubkin <konstantin.golubkin@hotmail.com>
2019-09-12 12:36:31 +0700

    replase jq to vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,86 @@
 <template>
   <div class="hello">
-    <button type="button" id="vkLogin">Войти</button>
-    <button type="button" id="vkUnLogin">Выйти</button>
-    <button type="button" id="vkGetPhotos">Получить фотографии</button>
+    <button
+      type="button"
+      @click="loginVK">Войти</button>
+    <button
+      type="button"
+      @click="logoutVK">Выйти</button>
+    <button
+      type="button"
+     @click="getPhotoVK">Получить фотографии</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld'
+  name: 'HelloWorld',
+  methods: {
+    loginVK () {
+      VK.Auth.login(
+      // callback-функция, которая будет вызвана после авторизации
+      function (response) {
+ 
+        console.log(response)
+ 
+        if (response.status === 'connected') { // авторизация прошла успешно
+ 
+          var user = response.session.user; //  информация о пользователе
+            /*
+             user.first_name - имя;
+             user.last_name - фамилия;
+             user.href - ссылка на страницу в формате https://vk.com/domain;
+             user.id  - идентификатор пользователя;
+             user.nickname -  отчество или никнейм (если указано);
+             */
+ 
+        } else if (response.status === 'not_authorized') { // пользователь авторизован в ВКонтакте, но не разрешил доступ приложению;
+ 
+        } else if (response.status === 'unknown') { // пользователь не авторизован ВКонтакте.
+ 
+        }
+ 
+      },
+      // права доступа (integer)
+      // допустимые значения:
+      // AUDIO:8
+      // FRIENDS:2
+      // MATCHES:32
+      // PHOTOS:4
+      // QUESTIONS:64
+      // VIDEO:16
+      // WIKI:128
+      VK.access.PHOTOS
+      );
+    },
+    logoutVK () {
+      VK.Auth.logout(function (response) {
+      console.log(response);
+      // {session: null, status: "unknown", settings: undefined}
+      });
+    },
+    getPhotoVK () {
+      VK.Api.call(
+      'photos.getAll', // название метода API https://vk.com/dev/methods
+      // параметры:
+      {
+        v: '5.73', // версия API (обязательный параметр)
+        count: 10 // количество фотографий
+      }, function (r) {
+ 
+        var count = r.response.count; // кол-во полученных фотографий
+        var items = r.response.items; // массив с фотографиями
+ 
+ 
+ 
+      });
+    }
+  },
+  mounted () {
+     VK.init({
+      apiId: 7132009
+    });
+  }
 }
 </script>
 
