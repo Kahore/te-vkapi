ee5e0f95a625c0141ac212b9634fc89bfdf033de
Konstantin Golubkin <konstantin.golubkin@hotmail.com>
2019-09-12 12:51:09 +0700

    get friends pt 1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@
       @click="logoutVK">Выйти</button>
     <button
       type="button"
-     @click="getPhotoVK">Получить фотографии</button>
+     @click="getPhotoVK">Получить список друзей</button>
   </div>
 </template>
 
@@ -19,13 +19,11 @@ export default {
     loginVK () {
       VK.Auth.login(
       // callback-функция, которая будет вызвана после авторизации
-      function (response) {
- 
-        console.log(response)
- 
-        if (response.status === 'connected') { // авторизация прошла успешно
- 
-          var user = response.session.user; //  информация о пользователе
+        function (response) {
+          console.log(response)
+
+          if (response.status === 'connected') { // авторизация прошла успешно
+            var user = response.session.user //  информация о пользователе
             /*
              user.first_name - имя;
              user.last_name - фамилия;
@@ -33,53 +31,50 @@ export default {
              user.id  - идентификатор пользователя;
              user.nickname -  отчество или никнейм (если указано);
              */
- 
-        } else if (response.status === 'not_authorized') { // пользователь авторизован в ВКонтакте, но не разрешил доступ приложению;
- 
-        } else if (response.status === 'unknown') { // пользователь не авторизован ВКонтакте.
- 
-        }
- 
-      },
-      // права доступа (integer)
-      // допустимые значения:
-      // AUDIO:8
-      // FRIENDS:2
-      // MATCHES:32
-      // PHOTOS:4
-      // QUESTIONS:64
-      // VIDEO:16
-      // WIKI:128
-      VK.access.PHOTOS
-      );
+            this.$store.dispatch('MUTATE_USER', user)
+          } else if (response.status === 'not_authorized') { // пользователь авторизован в ВКонтакте, но не разрешил доступ приложению;
+
+          } else if (response.status === 'unknown') { // пользователь не авторизован ВКонтакте.
+
+          }
+        },
+        // права доступа (integer)
+        // допустимые значения:
+        // AUDIO:8
+        // FRIENDS:2
+        // MATCHES:32
+        // PHOTOS:4
+        // QUESTIONS:64
+        // VIDEO:16
+        // WIKI:128
+        VK.access.FRIENDS
+      )
     },
     logoutVK () {
       VK.Auth.logout(function (response) {
-      console.log(response);
+        console.log(response)
       // {session: null, status: "unknown", settings: undefined}
-      });
+      })
     },
     getPhotoVK () {
+      const userInfo = this.$store.getters.userInfo
       VK.Api.call(
-      'photos.getAll', // название метода API https://vk.com/dev/methods
-      // параметры:
-      {
-        v: '5.73', // версия API (обязательный параметр)
-        count: 10 // количество фотографий
-      }, function (r) {
- 
-        var count = r.response.count; // кол-во полученных фотографий
-        var items = r.response.items; // массив с фотографиями
- 
- 
- 
-      });
+        'friends.getLists', // название метода API https://vk.com/dev/methods
+        // параметры:
+        {
+          v: '5.73', // версия API (обязательный параметр)
+          user_id: userInfo.id // количество фотографий
+        }, function (r) {
+          var count = r.response.count // кол-во полученных фотографий
+          var items = r.response.items // массив с фотографиями
+          console.log("TCL: getPhotoVK -> items", items)
+        })
     }
   },
   mounted () {
-     VK.init({
+    VK.init({
       apiId: 7132009
-    });
+    })
   }
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -5,12 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    userInfo: {}
+  },
+  getters: {
+    userInfo: state => {
+      return state.userInfo
+    }
   },
   mutations: {
-
+    MUTATE_USER (state, payload) {
+      state.user = payload
+    }
   },
   actions: {
-
+    MUTATE_USER ({ commit }, payload) {
+      commit('MUTATE_USER', payload)
+    }
   }
 })
